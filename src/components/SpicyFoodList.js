7f09fa4bf3346079c5215b11363fa938c51dd10d
Dennis Musembi <dennismusembi344@gmail.com>
2023-04-23 06:42:50 +0300

    completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,22 +3,61 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const[filterBy,setFilterBy]=useState("All")
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray=[...foods,newFood]
+    setFoods(newFoodArray)
+    // const newFoods=newFood.map((newFoo)=>
+    // <li key={newFoo.id}>{newFoo.name}|Heat:{newFoo.heatLevel}|Cuisine: {newFoo.cuisine}</li>)
+    // console.log(newFood);
+    // setFoods(newFoods)
   }
-
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  function handleClick(id){
+    //const newFoodArray = foods.filter((food)=>food.id!==id)
+    const newFoodArray=foods.map((food)=>{
+    if(food.id===id){
+      return{...food,heatLevel:food.heatLevel+1}
+    }
+    else{return food}
+  })
+  console.log(newFoodArray)
+    setFoods(newFoodArray)
+  }
+  function handleFilterChange(e){
+    setFilterBy(e.target.value)
+  }
+  const foodsToDisplay=foods.filter((food)=>{
+  if(filterBy==="All"){
+    return true;
+  }else{return food.cuisine===filterBy} 
+  })
+  // const foodList = foods.map((food) => (
+  //   <li onClick={()=>handleClick(food.id)} key={food.id}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // )
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+  
+
+
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFilterChange}>
+  <option value="All">All</option>
+  <option value="American">American</option>
+  <option value="Sichuan">Sichuan</option>
+  <option value="Thai">Thai</option>
+  <option value="Mexican">Mexican</option>
+</select>
     </div>
   );
 }
